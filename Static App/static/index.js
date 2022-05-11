b827927927d7f4d304d054efefeb8e1cc255d569
kiranu4u <54977954+kiranu4u@users.noreply.github.com>
2022-05-10 18:49:58 -0700

    Update index.js

--- Static App/static/index.js
+++ Static App/static/index.js
@@ -1,14 +1,14 @@
 //from parameters query full URL
 var shorturl = window.location.pathname.substr(1);
 var baseURL = "https://dev1-sre-apim.azure-api.net/shorturl/GetFullURL?alias=";
+var baseURL = "https://shorturlpoc.azurewebsites.net/api/GetFullURL?alias=";
 var requestURL = baseURL.concat(shorturl);
 var request = new XMLHttpRequest()
 request.open('GET',requestURL,false)
 //define request header
 //request.setRequestHeader("Ocp-Apim-Subscription-Key","<API key>")
-request.setRequestHeader('Access-Control-Allow-Origin','*')
-request.setRequestHeader('Access-Control-Allow-Methods','GET')
-
+//request.setRequestHeader('Access-Control-Allow-Origin','*')
+//request.setRequestHeader('Access-Control-Allow-Methods','GET')
 
 request.send();
 alert(request.responseText)
